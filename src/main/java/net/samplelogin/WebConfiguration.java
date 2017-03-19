931e86a29aa04226f87fe0242db043ca3e91a406
Oleksandr Hasheniuk <hasheniuk@gmail.com>
2017-03-19 15:18:41 +0200

    move view controllers to ViewControllerRegistry

---
+++ src/main/java/net/samplelogin/WebConfiguration.java
@@ -0,0 +1,15 @@
+package net.samplelogin;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.*;
+
+@Configuration
+public class WebConfiguration extends WebMvcConfigurerAdapter {
+    private static final String LOGIN_VIEW = "forward:login.html";
+
+    @Override
+    public void addViewControllers(final ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName(LOGIN_VIEW);
+        registry.addViewController("/login").setViewName(LOGIN_VIEW);
+    }
+}
